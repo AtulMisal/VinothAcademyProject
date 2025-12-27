a5d9a69450b7fe2e238bd884a53c4d7d2ac93f58
Atul <atulmisal69@gmail.com>
2025-12-27 17:15:08 +0530

    ABCD

--- src/test/java/runnerClass/RunnerFile.java
+++ src/test/java/runnerClass/RunnerFile.java
@@ -3,9 +3,9 @@ package runnerClass;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions  ( features = {"src/test/resources/Features/"},
-		            glue = { "stepDefinationFiles" }, 
-		            plugin = { "pretty", "html:target/Reports/cucumber-reports", "json:target/Reports/junit-reports/cucumber.xml",
+@CucumberOptions  ( features = {"C:/Users/atulm/Desktop/Selenium_Programs/VinothAcademyProject/src/test/resources/Features"},
+		            glue = {"stepDefinationFiles"}, 
+		            plugin = { "pretty", "html:target/Reports/cucumber-reports", "json:target/cucumber.json",
 				               "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				               "rerun:target/failedReRun.txt" }, 
 		            monochrome = true)
