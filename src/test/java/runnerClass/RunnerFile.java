0a489b335212def5828293eaa5122e5febcfcad5
Atul Misal <108976232+AtulMisal@users.noreply.github.com>
2025-12-27 17:50:15 +0530

    Update RunnerFile.java

--- src/test/java/runnerClass/RunnerFile.java
+++ src/test/java/runnerClass/RunnerFile.java
@@ -5,9 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions  ( features = {"C:/Users/atulm/Desktop/Selenium_Programs/VinothAcademyProject/src/test/resources/Features"},
 		            glue = {"stepDefinationFiles"}, 
-		            plugin = { "pretty", "html:target/cucumber-reports", "json:target/cucumber.json",
-				               "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				               "rerun:target/failedReRun.txt" }, 
+		            plugin = { "pretty",
+    						   "html:target/cucumber-reports",
+   							   "json:target/cucumber.json",
+   							   "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+    						   "rerun:target/failedReRun.txt"}, 
 		            monochrome = true)
 
 public class RunnerFile extends AbstractTestNGCucumberTests{
